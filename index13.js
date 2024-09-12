d84063043804a282337ad0059dff424884e7c93b
Paulo GS2024 <164101438+PGS2024@users.noreply.github.com>
2024-09-11 21:26:37 -0300

    Add files via upload

---
+++ index13.js
@@ -0,0 +1,18 @@
+ carro = "sedan"; 
+
+switch (carro) {
+    case "hatch":
+        console.log("Compra efetuada com sucesso");
+        break;
+    case "sedan":
+        console.log("Tem certeza que esse modelo?");
+        break;
+    case "motocicleta":
+        console.log("Tem certeza que esse modelo?");
+        break;
+     case "caminhonetes":
+            console.log("Tem certeza que esse modelo?");
+            break;
+    default:
+        console.log("Erro: Não trabalhamos com esse tipo de automóvel aqui.");
+}
